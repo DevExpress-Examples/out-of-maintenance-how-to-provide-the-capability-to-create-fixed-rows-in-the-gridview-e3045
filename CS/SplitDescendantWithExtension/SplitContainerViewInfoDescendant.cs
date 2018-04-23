49f09e80b92c7c2dcdeff6d99eb5ed691426f206
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/SplitDescendantWithExtension/SplitContainerViewInfoDescendant.cs
@@ -0,0 +1,31 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DevExpress.XtraEditors.Drawing;
+using System.Drawing;
+
+namespace GridViewFixedRowsSplitPanel {
+    public class SplitContainerViewInfoDescendant : SplitContainerViewInfo {
+        public SplitContainerViewInfoDescendant(DevExpress.XtraEditors.SplitContainerControl container)
+            : base(container) {
+
+        } 
+
+        protected override void CalcInfo() {
+            base.CalcInfo();
+        }
+        protected override void UpdatePanelBounds() {
+            base.UpdatePanelBounds();
+            Rectangle rect = Panel2Info.Bounds;
+            rect.Height += Splitter.Bounds.Height;
+            rect.Y -= Splitter.Bounds.Height;
+            Panel2Info.Bounds = rect;
+            rect = Splitter.Bounds;
+            rect.Height = 0;
+            Splitter.Bounds = rect;
+        }
+    }
+}
